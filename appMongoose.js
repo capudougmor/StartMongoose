ad869ecd58795db0b9e96a2f5fbcd324c4203488
Douglas Mendes <capudougmor@gmail.com>
2020-05-08 18:08:44 -0300

    Created model

--- appMongoose.js
+++ appMongoose.js
@@ -7,5 +7,41 @@ mongoose.connect("mongodb://localhost/appMongoose", {
 }).then(() => {
   console.log("Conectado no banco appMongoose")
 }).catch((err) => {
-  console.log("Erro ao conectar ao banco" + err)
+  console.log("Erro ao conectar ao banco: " + err)
+})
+
+//definindo o model
+const UserSchema = mongoose.Schema({
+  nome: {
+    type: String,
+    require: true
+  },
+  sobrenome: {
+    type: String,
+    require: true
+  },
+  idade: {
+    type: Number,
+    require:true
+  },
+  pais: {
+    type: String
+  }
+
+})
+
+//collection
+mongoose.model('usuarios', UserSchema)
+
+const Douglas = mongoose.model('usuarios')
+
+new Douglas({
+  nome: "Douglas",
+  sobrenome: "Mendes",
+  idade: 35,
+  pais: "Brasil"
+}).save().then(() => {
+  console.log("Usuario criado")
+}).catch((err) => {
+  console.log("Erro ao cadastrar usuario: " +err)
 })
